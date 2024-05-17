a942332345c6205b2e226c3e728190711bf509dc
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-05-17 17:55:10 +0500

    214. Slider - Current Item and Prev/Next

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,11 +4,22 @@ import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(list);
+  const [people, setPeople] = useState(longList);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
-  const prevSlide = () => {};
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
 
-  const nextSlide = () => {};
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
 
   return (
     <section className="slider-container">
@@ -17,7 +28,11 @@ const Carousel = () => {
         return (
           <article
             className="slide"
-            style={{ transform: `translateX(${100 * personIndex}%)` }}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? "visible" : "hidden",
+            }}
             key={id}
           >
             <img src={image} alt={name} className="person-img" />
