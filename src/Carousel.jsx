6038ca291db7d0b16a987fdd0575e96317746aec
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-05-21 17:13:51 +0500

    completed

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -32,27 +32,6 @@ const Carousel = () => {
 
   return (
     <section className="slider-container">
-      {people.map((person, personIndex) => {
-        const { id, image, name, title, quote } = person;
-        return (
-          <article
-            className="slide"
-            style={{
-              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
-              opacity: personIndex === currentPerson ? 1 : 0,
-              visibility: personIndex === currentPerson ? "visible" : "hidden",
-            }}
-            key={id}
-          >
-            <img src={image} alt={name} className="person-img" />
-            <h5 className="name">{name}</h5>
-            <p className="title">{title}</p>
-            <p className="text">{quote}</p>
-            <FaQuoteRight className="icon" />
-          </article>
-        );
-      })}
-
       <button type="button" onClick={prevSlide} className="prev">
         <FiChevronLeft />
       </button>
